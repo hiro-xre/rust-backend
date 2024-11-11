ec15de1cf38a124354a5fa928c59250f5263a6a1
hiroto-hara <xre0605@gmail.com>
2024-11-11 21:07:57 +0900

    加減算

--- chapter3/arithmetic-operations-quiz/src/main.rs
+++ chapter3/arithmetic-operations-quiz/src/main.rs
@@ -1,7 +1,44 @@
+use rand::Rng;
+
 fn main() {
-    println!("1 + 1 = ??");
+    // 加算
+    let op1 = rand::thread_rng().gen_range(0..100);
+    let op2 = rand::thread_rng().gen_range(0..100);
+    println!("{} + {} = ??", op1, op2);
     println!("?? の値を入力してください:");
+    // ユーザーからの回答を保持する変数
     let mut ans_input = String::new();
+    // 標準入力から1行取得し,ans_inputに代入
     std::io::stdin().read_line(&mut ans_input).unwrap();
+    // ans_inputからtrim()で改行を取り除き,parse()で整数に変換
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
+
     dbg!(ans_input);
+    if dbg!(ans_input == op1 + op2) {
+        println!("正解!");
+    } else {
+        println!("不正解!");
+    }
+
+    // 減算
+    let op1 = rand::thread_rng().gen_range(0..100);
+    let op2 = rand::thread_rng().gen_range(0..100);
+    println!("{} - {} = ??", op1, op2);
+    println!("?? の値を入力してください:");
+
+    // ユーザーからの回答を保持する変数
+    let mut ans_input = String::new();
+
+    // 標準入力から1行取得し,ans_inputに代入
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+
+    // ans_inputからtrim()で改行を取り除き,parse()で整数に変換
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
+
+    dbg!(ans_input);
+    if dbg!(ans_input == op1 - op2) {
+        println!("正解!");
+    } else {
+        println!("不正解!");
+    }
 }
