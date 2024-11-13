c17f7d5f83581a863daa6031f8074f0bdda8c745
hiroto-hara <xre0605@gmail.com>
2024-11-13 22:00:52 +0900

    山札の作成

---
+++ chapter4/simple-poker/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Suit {
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Card {
+    suit: Suit,
+    rank: i32,
+}
+
+use rand::seq::SliceRandom;
+fn main() {
+    let mut deck: Vec<Card> = Vec::new();
+    let suits = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade];
+
+    for suit in suits {
+        for rank in 1..=13 {
+            deck.push(Card { suit, rank });
+        }
+    }
+    let mut rng = rand::thread_rng();
+    deck.shuffle(&mut rng);
+    println!("{:?}", deck);
+}
