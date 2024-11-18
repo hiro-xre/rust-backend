69c4da7132a033fc03d81d64754e8a0ec1f59580
hiroto-hara <xre0605@gmail.com>
2024-11-18 21:17:13 +0900

    ポーカーゲームの実装完了

--- chapter4/simple-poker/src/main.rs
+++ chapter4/simple-poker/src/main.rs
@@ -24,5 +24,59 @@ fn main() {
     }
     let mut rng = rand::thread_rng();
     deck.shuffle(&mut rng);
-    println!("{:?}", deck);
+
+    // 手札
+    let mut hand: Vec<Card> = Vec::new();
+    // 5枚引く
+    for _ in 0..5 {
+        hand.push(deck.pop().unwrap());
+    }
+    // 手札をソート
+    hand.sort_by(|a, b| a.rank.cmp(&b.rank));
+    // 手札を表示
+    println!("---hand---");
+    for (i, card) in hand.iter().enumerate() {
+        println!("{:}: {:?} {:}", i + 1, card.suit, card.rank)
+    }
+
+    // 手札交換
+    println!("入れ替えたいカードの番号を入力してください(例: 1 2 3)");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let numbers: Vec<usize> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<usize>>();
+    for number in numbers {
+        hand[number - 1] = deck.pop().unwrap();
+    }
+    hand.sort_by(|a, b| a.rank.cmp(&b.rank));
+    println!("---hand---");
+    for (i, card) in hand.iter().enumerate() {
+        println!("{:}: {:?} {:}", i + 1, card.suit, card.rank)
+    }
+
+    // 役判定
+    let suit = hand.first().unwrap().suit;
+    let flash = hand.iter().all(|c| c.suit == suit);
+    let mut count = 0;
+    for i in 0..hand.len() - 1 {
+        for j in i + 1..hand.len() {
+            if hand[i].rank == hand[j].rank {
+                count += 1;
+            }
+        }
+    }
+
+    if flash {
+        println!("フラッシュ!");
+    } else if count >= 3 {
+        println!("スリーカード!")
+    } else if count == 2 {
+        println!("2ペア!");
+    } else if count == 1 {
+        println!("1ペア!");
+    } else {
+        println!("役なし...")
+    }
 }
